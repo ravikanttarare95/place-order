d7f0f4c6409b584ee858bc8eb0a9180664281f25
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-05-02 16:11:38 +0530

    add functionality to Next Product button

--- script.js
+++ script.js
@@ -1,16 +1,89 @@
-const productMainImage = document.getElementById("product-main-img");
+function toggleLightMode() {
+  const lightModeIcon = document.getElementById("light-mode-icon");
+  if (lightModeIcon.src.includes("light-mode.png")) {
+    lightModeIcon.src = "./image/dark-mode.png";
+  } else if (lightModeIcon.src.includes("dark-mode.png")) {
+    lightModeIcon.src = "./image/light-mode.png";
+  }
+}
+const productImage1 = document.getElementById("shoe-1");
+const productImage2 = document.getElementById("shoe-2");
+const productImage3 = document.getElementById("shoe-3");
+const productImage4 = document.getElementById("shoe-4");
 
-function changeProductImage1(imageSrc) {
-  productMainImage.src = "./image/shoe-1.jpg";
+function showProductImage1(imageSrc) {
+  const productMainImage = document.getElementById("product-main-img");
+
+  if (productImage1.src.includes("shoe-1.jpg")) {
+    productMainImage.src = "./image/shoe-1.jpg";
+  } else if (productImage1.src.includes("mobile-1.jpg")) {
+    productMainImage.src = "./image/mobile-1.jpg";
+  } else if (productImage1.src.includes("camera-1.png")) {
+    productMainImage.src = "./image/camera-1.png";
+  }
+
+  //   productMainImage.src = "./image/shoe-1.jpg";
 }
-function changeProductImage2(imageSrc) {
-  productMainImage.src = "./image/shoe-2.jpg";
+function showProductImage2(imageSrc) {
+  const productMainImage = document.getElementById("product-main-img");
+  if (productImage2.src.includes("shoe-2.jpg")) {
+    productMainImage.src = "./image/shoe-2.jpg";
+  } else if (productImage2.src.includes("mobile-2.png")) {
+    productMainImage.src = "./image/mobile-2.png";
+  } else if (productImage2.src.includes("camera-2.png")) {
+    productMainImage.src = "./image/camera-2.png";
+  }
 }
-function changeProductImage3(imageSrc) {
-  productMainImage.src = "./image/shoe-3.jpg";
+function showProductImage3(imageSrc) {
+  const productMainImage = document.getElementById("product-main-img");
+  if (productImage3.src.includes("shoe-3.jpg")) {
+    productMainImage.src = "./image/shoe-3.jpg";
+  } else if (productImage3.src.includes("mobile-3.jpeg")) {
+    productMainImage.src = "./image/mobile-3.jpeg";
+  } else if (productImage3.src.includes("camera-3.png")) {
+    productMainImage.src = "./image/camera-3.png";
+  }
 }
-function changeProductImage4(imageSrc) {
-  productMainImage.src = "./image/shoe-4.jpg";
+function showProductImage4(imageSrc) {
+  const productMainImage = document.getElementById("product-main-img");
+  if (productImage4.src.includes("shoe-4.jpg")) {
+    productMainImage.src = "./image/shoe-4.jpg";
+  } else if (productImage4.src.includes("mobile-4.png")) {
+    productMainImage.src = "./image/mobile-4.png";
+  } else if (productImage4.src.includes("camera-4.png")) {
+    productMainImage.src = "./image/camera-4.png";
+  }
+}
+
+function nextProductImage() {
+  const productMainImage = document.getElementById("product-main-img");
+  const productImage1 = document.getElementById("shoe-1");
+  const productImage2 = document.getElementById("shoe-2");
+  const productImage3 = document.getElementById("shoe-3");
+  const productImage4 = document.getElementById("shoe-4");
+  if (
+    productMainImage.src.includes("shoe-1.jpg") ||
+    productMainImage.src.includes("shoe-2.jpg") ||
+    productMainImage.src.includes("shoe-3.jpg") ||
+    productMainImage.src.includes("shoe-4.jpg")
+  ) {
+    productMainImage.src = "./image/mobile-1.jpg";
+    productImage1.src = "./image/mobile-1.jpg";
+    productImage2.src = "./image/mobile-2.png";
+    productImage3.src = "./image/mobile-3.jpeg";
+    productImage4.src = "./image/mobile-4.png";
+  } else if (
+    productMainImage.src.includes("mobile-1.jpg") ||
+    productMainImage.src.includes("mobile-2.png") ||
+    productMainImage.src.includes("mobile-3.jpeg") ||
+    productMainImage.src.includes("mobile-4.png")
+  ) {
+    productMainImage.src = "./image/camera-1.png";
+    productImage1.src = "./image/camera-1.png";
+    productImage2.src = "./image/camera-2.png";
+    productImage3.src = "./image/camera-3.png";
+    productImage4.src = "./image/camera-4.png";
+  }
 }
 
 const starImages1 = document.getElementById("star-1-img");
